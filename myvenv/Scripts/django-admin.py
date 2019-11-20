6d886d6e3b8ea740797c6d3d3a624a87201888a8
Leeseonha <jmtkdsh@gmail.com>
2019-11-20 19:53:09 +0900

    channels

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\이선하\desktop\멋사\2019_2학기 멋사실습\django_channels\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
